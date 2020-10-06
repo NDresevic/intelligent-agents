804ea9f1c414590ec77f0041d9a937ce2c71ae2d
dkutlesic <dubravka97@gmail.com>
2020-10-06 15:23:17 +0200

    Print agent id; reactive xml comments removed

--- workspace/322505-321682-ex2/src/agents/ReactiveAgent.java
+++ workspace/322505-321682-ex2/src/agents/ReactiveAgent.java
@@ -268,7 +268,7 @@ public class ReactiveAgent implements ReactiveBehavior {
         }
 
         if (numActions >= 1) {
-            System.out.println("The total profit after " + numActions + " actions is " + myAgent.getTotalProfit() +
+            System.out.println("Agent id " + myAgent.id() + ": The total profit after " + numActions + " actions is " + myAgent.getTotalProfit() +
                     " (average profit: " + (myAgent.getTotalProfit() / (double) numActions) + ")");
         }
         numActions++;
