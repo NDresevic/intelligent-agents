8a96bc2618133ed062d3ca4b8114936c309b5c7a
dkutlesic <dubravka97@gmail.com>
2020-10-16 00:11:44 +0200

    BFS

--- workspace/322505-321682-ex3/src/deliberative/BFS.java
+++ workspace/322505-321682-ex3/src/deliberative/BFS.java
@@ -8,8 +8,13 @@ import java.util.*;
 
 public class BFS extends SearchAlgorithm {
 
+    List<State> optimalPath;
+    private final Map<State, State> parentOptimal;
+
     public BFS(Set<Task> availableTaskSet, Set<Task> carriedTaskSet, Topology topology, Vehicle vehicle) {
         super(availableTaskSet, carriedTaskSet, topology, vehicle);
+        this.parentOptimal = new HashMap<>();
+        this.optimalPath = new ArrayList<>();
     }
 
     @Override
@@ -26,20 +31,27 @@ public class BFS extends SearchAlgorithm {
     private State breadthFirstTraversal(State rootState) {
         Set<State> visited = new LinkedHashSet<>();
         Queue<State> queue = new LinkedList<>();
-        double minCostFromRoot = Double.MAX_VALUE;
-        State optimalGoalState = null;
+        State goalState = null;
+        double costFromRoot = Double.MAX_VALUE;
 
         queue.add(rootState);
         visited.add(rootState);
+        G.put(rootState, 0d);
         while (!queue.isEmpty()) {
             State currentState = queue.poll();
-
-//            if (currentState.isGoalState() && currentState.getCostFromRoot() < minCostFromRoot) {
-//                minCostFromRoot = currentState.getCostFromRoot();
-//                optimalGoalState = currentState;
-//            }
-
+            if (currentState.isGoalState()) {
+                if(G.get(currentState) < costFromRoot) {
+                    costFromRoot = G.get(currentState);
+                    goalState = currentState;
+                }
+            }
             for (State child : currentState.getChildren()) {
+                G.putIfAbsent(child, Double.MAX_VALUE);
+                double possiblyShorterPath = G.get(currentState) + currentState.getCurrentCity().distanceTo(child.getCurrentCity());
+                if (possiblyShorterPath < G.get(child)) {
+                    G.put(child, possiblyShorterPath);
+                    parentOptimal.put(child, currentState);
+                }
                 if (!visited.contains(child)) {
                     visited.add(child);
                     queue.add(child);
@@ -47,10 +59,23 @@ public class BFS extends SearchAlgorithm {
             }
         }
 
-        System.out.println("BROJ STANJA: " + visited.size());
-        //return visited;
+        if (goalState != null) {
+            if (goalState != null) {
+                State currentState = goalState;
+                while (!currentState.equals(rootState)) {
+                    optimalPath.add(currentState);
+                    currentState = parentOptimal.get(currentState);
+                }
+            }
+            optimalPath.add(rootState);
+        }
+        Collections.reverse(optimalPath);
 
-//        System.out.println(visited);
-        return optimalGoalState;
+        for(State s : optimalPath)
+            System.out.println(s + "\n");
+
+        System.out.println("BROJ STANJA: " + visited.size());
+
+        return goalState;
     }
 }
