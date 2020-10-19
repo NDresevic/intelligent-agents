8b3f42a79def1cf61a97e4dda1fa2cac18a94f4c
dkutlesic <dubravka97@gmail.com>
2020-10-19 15:18:05 +0200

    Qin - remember who is in Q

--- workspace/322505-321682-ex3/src/deliberative/AStar.java
+++ workspace/322505-321682-ex3/src/deliberative/AStar.java
@@ -3,22 +3,23 @@ package deliberative;
 import logist.simulation.Vehicle;
 import logist.task.Task;
 
-import java.util.*;
-
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.PriorityQueue;
 import java.util.Set;
 
 public class AStar extends SearchAlgorithm {
 
     private final PriorityQueue<State> Q;
     private final Set<State> C;
-    private final Map<State, Double> F;
+    private final Set<State> Qin;
 
     public AStar(Set<Task> availableTaskSet, Set<Task> carriedTaskSet, Vehicle vehicle) {
         super(availableTaskSet, carriedTaskSet, vehicle);
 
         this.Q = new PriorityQueue<>(Comparator.comparingDouble(this::calculateF));
         this.C = new HashSet<>();
-        this.F = new HashMap<>();
+        this.Qin = new HashSet<>();
     }
 
     private Double calculateF(State state) {
@@ -27,39 +28,43 @@ public class AStar extends SearchAlgorithm {
 
     @Override
     public State getGoalState() {
+        System.err.println("get goal state start");
         G.put(rootState, 0d);
-        F.put(rootState, calculateF(rootState));
         Q.add(rootState);
+        Qin.add(rootState);
 
         State currentState;
         // while there are unprocessed states
         while (!Q.isEmpty()) {
             visitedStates++;
             currentState = Q.poll();
-            C.add(currentState);
 
             if (currentState.isFinalState()) {
+                System.err.println("get goal state finish");
                 return currentState;
             }
+
+            C.add(currentState);
 
             // process children
             for (State child : currentState.getChildren()) {
                 double pathOverCurrentState = G.get(currentState) +
                         currentState.getCurrentCity().distanceTo(child.getCurrentCity());
-                if (!Q.contains(child) && !C.contains(child)) {
+                if (!C.contains(child) && !Qin.contains(child)) {
                     G.put(child, pathOverCurrentState);
-                    F.put(child, calculateF(child));
                     Q.add(child);
+                    Qin.add(child);
                     parentOptimal.put(child, currentState);
                 } // another new path to child
                 // path to child over current state is better than the previous optimal path to child
                 else if (pathOverCurrentState < G.get(child)) {
-                    // not removing because of the time complexity
-//                    Q.remove(child);
+                    Q.remove(child);
                     G.put(child, pathOverCurrentState);
-                    F.put(child, calculateF(child));
-                    Q.add(child);
                     parentOptimal.put(child, currentState);
+//                    if(C.contains(child)){
+//                        C.remove(child);
+//                    }
+                    Q.add(child);
                 }
             }
         }
